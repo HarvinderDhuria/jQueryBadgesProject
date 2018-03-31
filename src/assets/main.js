c426e1b7b60d643a92f788d3071a889dc44769c2
Harvinder Singh <harvinderdhuria@outlook.com>
2018-03-31 11:44:26 +0530

    Initial commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,24 @@
-$(function() {
 
-  // your code will go here
+$(document).ready(function(){
+  var url="https://www.codeschool.com/users/manvindersingh.json";
+  $.ajax(url,{
+    dataType:"jsonp",
+    success:function(response){
+      console.log("succeed "+response);
+      console.log(response.badges);
+      console.log(response.courses.completed);
 
+      var badges=$("#badges");
+      var count=response.courses.completed.length;
+      for(var i=0;i<count;i++){
+        var currentCourse =  response.courses.completed[i];
+        var title="<h3>" + currentCourse.title + "</h3>";
+        var image = "<img src='" + currentCourse.badge +  "' alt='" + currentCourse.title + "'/>";
+        var linkElement = "<a class='btn btn-primary' href='" + currentCourse.url + "' target=_blank>See Course</a>";
+
+        var div="<div class='course'>" + title + image +  linkElement + "</div";
+        $("#badges").append(div);
+      }
+    },
+  });
 });
